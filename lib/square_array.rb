e089fa94b3be1c708684d732e4d7372d68235534
Blaine R Love <blainelove94@gmail.com>
2020-12-30 17:01:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,8 @@
 
 def square_array(array)
   counter = 0
-  final = []
   while array[counter] do
-    final.push(array[counter]*array[counter])
+    puts array[counter]*array[counter]
     counter += 1
   end
-  return final
 end
